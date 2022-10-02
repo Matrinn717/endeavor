5b4cd4fc9d845d54e2f951d4c3ef50cc17f66ae4
Matrinn717 <114235694+Matrinn717@users.noreply.github.com>
2022-10-02 20:10:33 +0800

    Add files via upload

---
+++ upsidedownLinkedList.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *cur =head, *pre=nullptr;
+        while(cur != nullptr){
+            ListNode* tmp=cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+};
